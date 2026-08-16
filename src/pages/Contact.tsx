084f30e26c4424f9ee8f5863b1b045e5cf2ad84f
boltforwork9 <boltforwork9@gmail.com>
2026-08-16 12:02:02 +0300

    Update Contact page header background

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
   return (
     <PageTransition>
       {/* ===== Hero Header ===== */}
-      <section className="relative w-full overflow-hidden bg-ink py-24 lg:py-28">
+      <section className="relative w-full overflow-hidden bg-[url('/images/image.png')] bg-cover bg-center bg-no-repeat py-24 lg:py-28">
         <div className="pointer-events-none absolute -left-24 top-1/4 h-72 w-72 rounded-full bg-[#1B753C]/25 blur-[120px]" />
         <div className="pointer-events-none absolute -right-16 bottom-0 h-80 w-80 rounded-full bg-[#B32025]/20 blur-[130px]" />
 
