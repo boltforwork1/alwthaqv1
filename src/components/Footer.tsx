e49be7b157a20c49a561e845a4139c0105f09ed2
boltforwork14 <boltforwork14@gmail.com>
2026-08-16 12:09:28 +0300

    Enlarge footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
             <img
               src="/logo.png"
               alt="Al Wthaq Group"
-              className="h-24 w-auto object-contain sm:h-24 lg:h-[100px]"
+              className="h-28 w-auto object-contain sm:h-32 lg:h-36"
             />
           </Link>
           <p className="mt-5 max-w-xs text-sm leading-7 text-ink/60">
